a756e38a8e4f0ccf603a2c3d884e3a537b8efde7
Hannu Salo <hannu_salo@hotmail.com>
2023-04-28 11:59:45 +0300

    feat: added social-icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,29 @@
 import React from "react";
+import instagramIcon from "../../public/images/icons/instagram.png"
+import twitterIcon from "../../public/images/icons/twitter.png"
+import youtubeIcon from "../../public/images/icons/youtube.png"
+import spotifyIcon from "../../public/images/icons/spotify.png"
+import soundcloudIcon from "../../public/images/icons/soundcloud.png"
+import Image from "next/image";
 
 const Footer = () => {
     return (
         <footer className='bg-neutral-200 container mx-auto pb-10 pt-10 mt-40 items-center flex flex-col'>
-            <nav className='flex flex-col lg:flex-row gap-4 text-neutral-500 font-medium uppercase'>
-                <a href="/" target={"_blank"}>
-                Instagram
+            <nav className='flex flex-row gap-4 text-neutral-500 font-medium uppercase'>
+                <a href="https://instagram.com/hannsalmusic" target={"_blank"}>
+                <Image src={instagramIcon} alt="HANNSAL instagram" className="drop-shadow" />
+                </a>
+                <a href="https://twitter.com/hannsalmusic" target={"_blank"}>
+                <Image src={twitterIcon} alt="HANNSAL twitter" className="drop-shadow" />
+                </a>
+                <a href="https://www.youtube.com/channel/UCT1Cqpnduia-KgqzGYm8K0g" target={"_blank"}>
+                <Image src={youtubeIcon} alt="HANNSAL youtube" className="drop-shadow" />
                 </a>
                 <a href="/" target={"_blank"}>
-                Twitter
+                <Image src={spotifyIcon} alt="HANNSAL spotify" className="drop-shadow" />
                 </a>
-                <a href="/" target={"_blank"}>
-                Youtube
-                </a>
-                <a href="/" target={"_blank"}>
-                Spotify
-                </a>
-                <a href="/" target={"_blank"}>
-                Soundcloud
+                <a href="https://soundcloud.com/hannsal" target={"_blank"}>
+                <Image src={soundcloudIcon} alt="HANNSAL soundcloud" className="drop-shadow" />
                 </a>
             </nav>
             <p className="text-neutral-500 mt-5 text-xs uppercase">&copy; 2023 | HANNSALmusic |  All rights reserved</p>
