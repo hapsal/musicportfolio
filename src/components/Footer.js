506cf61a4467baca6162899e3252b475a1320976
Hannu Salo <hannu_salo@hotmail.com>
2023-05-03 14:24:20 +0300

    fix: footer-links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from 'next/link'
 import instagramIcon from "../../public/images/icons/instagram.png"
 import twitterIcon from "../../public/images/icons/twitter.png"
 import youtubeIcon from "../../public/images/icons/youtube.png"
@@ -10,21 +11,21 @@ const Footer = () => {
     return (
         <footer className='bg-neutral-200 container mx-auto pb-10 pt-10 mt-40 items-center flex flex-col'>
             <nav className='flex flex-row gap-4 text-neutral-500 font-medium uppercase'>
-                <a href="https://instagram.com/hannsalmusic" target={"_blank"}>
-                <Image src={instagramIcon} alt="HANNSAL instagram" className="drop-shadow" />
-                </a>
-                <a href="https://twitter.com/hannsalmusic" target={"_blank"}>
-                <Image src={twitterIcon} alt="HANNSAL twitter" className="drop-shadow" />
-                </a>
-                <a href="https://www.youtube.com/channel/UCT1Cqpnduia-KgqzGYm8K0g" target={"_blank"}>
-                <Image src={youtubeIcon} alt="HANNSAL youtube" className="drop-shadow" />
-                </a>
-                <a href="/" target={"_blank"}>
-                <Image src={spotifyIcon} alt="HANNSAL spotify" className="drop-shadow" />
-                </a>
-                <a href="https://soundcloud.com/hannsal" target={"_blank"}>
-                <Image src={soundcloudIcon} alt="HANNSAL soundcloud" className="drop-shadow" />
-                </a>
+                <Link href="https://instagram.com/hannsalmusic" target={"_blank"}>
+                    <Image src={instagramIcon} alt="HANNSAL instagram" className="drop-shadow" />
+                </Link>
+                <Link href="https://twitter.com/hannsalmusic" target={"_blank"}>
+                    <Image src={twitterIcon} alt="HANNSAL twitter" className="drop-shadow" />
+                </Link>
+                <Link href="https://www.youtube.com/channel/UCT1Cqpnduia-KgqzGYm8K0g" target={"_blank"}>
+                    <Image src={youtubeIcon} alt="HANNSAL youtube" className="drop-shadow" />
+                </Link>
+                <Link href="/" target={"_blank"}>
+                    <Image src={spotifyIcon} alt="HANNSAL spotify" className="drop-shadow" />
+                </Link>
+                <Link href="https://soundcloud.com/hannsal" target={"_blank"}>
+                    <Image src={soundcloudIcon} alt="HANNSAL soundcloud" className="drop-shadow" />
+                </Link>
             </nav>
             <p className="text-neutral-500 mt-5 text-xs uppercase">&copy; 2023 | HANNSALmusic |  All rights reserved</p>
         </footer>
--- src/pages/music.js
+++ src/pages/music.js
@@ -18,7 +18,7 @@ const Music = () => {
                 <li key={song.id} className="text-center">
                     <img src={song.artwork["480x480"]} alt={song.description} className="rounded-md drop-shadow mb-4" />
                     <a className="text-neutral-900 lg:text-lg font-semibold drop-shadow hover:text-sky-400" href={`https://audius.co` + song.permalink} target={"_blank"}>
-                        <motion.div whileHover={{scale: 1.2}} transition={{type: "spring", stiffness: 400}}>"{song.title}"</motion.div></a>
+                        <motion.div whileHover={{scale: 1.2}} transition={{type: "spring", stiffness: 400}}>&quot;{song.title}&quot;</motion.div></a>
                 </li>
                 ))}
             </ul>
